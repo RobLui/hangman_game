76a9414b1d125a98a697c3d6d76ea670324c62aa
Robbert Luit <robbertluit@hotmail.com>
2015-10-29 13:38:09 +0100

    hangman_test_build try

---
+++ hangman/Assets/Scripts/a_controller.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class a_controller : MonoBehaviour {
+
+    //Toegang tot het model vanuit anderen die via de controller waardes uit het model nodig hebben
+    public static a_model AccessToModel = new a_model();
+    //Toegang tot de controller vanuit andere klasses
+    public static a_controller AccessToController = new a_controller();
+
+    // Initialisatie
+    void Start()
+    {
+        //Bij het starten van het spel creëer een woord om te raden
+        GenerateRandomWord();
+    }
+
+    //Genereer een random woord om te raden
+    public void GenerateRandomWord() 
+    {
+        //Waarde van Chosen_word = random woord
+        AccessToModel.Chosen_word = a_model.wordList[PickWord()];
+        //Guessed word (aantal streepjes voor het verborgen woord) wordt gelijk gezet aan het random woord
+        AccessToModel.GuessedWord = new string("-"[0], AccessToModel.Chosen_word.Length);
+        //Spiekbriefje voor in de console te kunnen zien wat het woord dat geraden moet worden is
+        Debug.Log(AccessToModel.Chosen_word);
+    }
+
+    //Genereer een random woord op basis van de lijst grootte
+    public int PickWord()
+    {
+        //Beginwaarde op 0
+        int pick_random_nbr = 0;
+        //Geef een random nummer terug die tussen de 0 en het de maximale hoeveelheid woorden in de List ligt
+        pick_random_nbr = Random.Range(0, a_model.wordList.Count);
+        //Geef dit nummer terug
+        return pick_random_nbr;
+    }
+
+    //Check of het ingevoerde karakter in het te raden woord zit
+    public void checkCharacter(char a)
+    {
+        //Bij oproepen functie is de karakter nog niet bestaande (hier vertrekken we van)
+        bool charExists = false;
+        
+        //Loopen door elke letter van het gekozen woord
+        for (int x = 0; x < AccessToModel.Chosen_word.Length; x++)
+        {
+            //Als karakter a gelijk is aan een letter in het woord dan..
+            if (AccessToModel.Chosen_word[x] == a)
+            {
+                //Zetten we karakter bestaat op true
+                charExists = true;
+                //Gaan we een tijdelijke string aanmaken die het woord splitst en de letter op de plaats zet waar hij zijn gelijke letter terugvind
+                string temp = AccessToModel.GuessedWord.Substring(0, x);
+                //Zet guessedWord gelijk aan de toegevoegde letter (op de plaats waar hij vandaan komt) en voeg de rest van de letters weer toe als "-" in het woord
+                AccessToModel.GuessedWord = temp + a.ToString() + AccessToModel.GuessedWord.Substring(x + 1, AccessToModel.GuessedWord.Length - x - 1);
+            }
+        }
+        // Foute letter geraden = Als de karakter niet bestaat in het te zoeken woord dan..
+        if (!charExists)
+        {
+            //Verwijder een leven
+            AccessToModel.Lives--;
+
+            //Laat nieuw deel van de man zien
+            a_controller.AccessToController.SpawnFoto();
+
+            //Laat de levens zien die nog over zijn in de console (debug functie)
+            //Debug.Log(AccessToModel.Lives);
+        }
+    }
+
+    //Voeg score toe
+    public int AddScore()
+    {
+            //Tel een punt bij
+            AccessToModel.Score++;
+            
+            //Return de waarde die Score op na toevoeging heeft
+            return AccessToModel.Score;
+    }
+
+    //Stop het spel
+    public void EndGame() //----DIT MOET NOG AANGEPAST WORDEN----
+    {
+        //Als de bool still_alive false is dan ... 
+        if (AccessToModel.Still_alive == false)
+        {
+            //Spawn foto met hangende man
+            GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("leftleg");
+        }
+    }
+    
+    //Spawn foto van hangende man
+    public void SpawnFoto()
+    {
+        switch (a_controller.AccessToModel.Counter)
+        {
+            case 0:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("hung1");
+                break;
+            case 1:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("hung2");
+                break;
+            case 2:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("shirt");
+                break;
+            case 3:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("rightarm");
+                break;
+            case 4:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("leftarm");
+                break;
+            case 5:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("rightleg");
+                break;
+            case 6:
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("leftleg");
+                break;             
+        }
+        AccessToModel.Counter++;
+    }
+
+    //Check of er nog voldoende levens zijn om verder te spelen
+    public void CheckForLives()
+    {
+        if (AccessToModel.Lives < 0)
+        {
+            //Zet still_alive op vals
+            AccessToModel.Still_alive = false;
+            //Voer EndGame() uit
+            EndGame();
+        }
+    }
+
+    //Als het woord gelijk is aan het gezochte woord voeg dan score toe
+    public void WordIsRight()
+    {
+        //Vergelijk het ingevoerde woord met het te raden woord
+        if (AccessToModel.GuessedWord == AccessToModel.Chosen_word)
+        {
+            //Voeg score toe
+            AddScore();
+            //Genereer een nieuw random woord
+            GenerateRandomWord();
+        }
+    }
+
+    //Reset de counter naar 0
+    public void CounterZero()
+    {
+        //Zet de waarde van counter op 0, zorgt ervoor dat de foto's terug van 0 beginnen
+        a_controller.AccessToModel.Counter = 0;
+        a_controller.AccessToModel.Score = 0;
+    }
+    
+}
---
+++ hangman/Assets/Scripts/a_view.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class a_view : MonoBehaviour {
+
+    private a_model a = a_controller.AccessToModel;
+    // public GUIStyle stylingGUI;
+    public GUISkin stylingSkin;
+
+    public void OnGUI()
+    {
+        GUI.skin = stylingSkin;
+
+
+        //De tijd wordt gelijk gesteld aan uren / minuten / seconden, waar we - doen voor elke second die voorbij gaat 
+        a_controller.AccessToModel.Time -= Time.deltaTime;
+
+        //********************************** TIJD **************************************//
+        //Als de tijd groter is dan 0 dan..
+        if (a_controller.AccessToModel.Time > 0)
+        {
+        //show de tijd die je nog over hebt in GUI (unity scherm)
+            GUI.Label(new Rect(100, 100, 200, 200), "Je hebt nog " + ((int)a_controller.AccessToModel.Time + " seconden"));
+        }
+        else
+        {
+        //Verander het label naar andere text op dezelfde plaats in GUI
+            GUI.Label(new Rect(100, 100, 100, 100), "Je tijd is om, you lose");
+            //waarde van levend/dood wordt op "dood" gezet als de tijd over is
+            a.Still_alive = false;
+            //stop het spel als de tijd om is ------ NOG AAN TE MAKEN ------
+            a_controller.AccessToController.EndGame();
+        }
+
+
+
+
+        //********************************** POSITIE BOXEN **************************************//
+
+        //Positie van de Positie waar de streepjes van het te raden woord in moeten komen
+        Rect BoxPosition = new Rect(500, 50, 200, 50);
+        GUI.Box(BoxPosition, a.GuessedWord);
+        
+        //Positie van de textField, input die van de user kan komen
+        Rect textFieldPosition = new Rect(500, 100, 200, 100);
+        a.UserInput = GUI.TextField(textFieldPosition, a.UserInput);
+
+
+
+
+
+        //********************************** VERGELIJKING **************************************//
+
+        //Als het gekozen woord volledig gelijk is aan het te raden woord dan..
+        a_controller.AccessToController.WordIsRight();
+
+        //Zet het label van score op de waardes
+        GUI.Label(new Rect(900, 100, 200, 200), ("Score: " + a_controller.AccessToModel.Score));
+
+
+
+
+        //********************************** BUTTON **************************************//
+
+        //Positie van de drukknop
+        Rect buttonPosition = new Rect(500, 200, 200, 100);
+
+        //Als er op de knop wordt gedrukt dan... (Op de knop staat "Probeer")
+        if (GUI.Button(buttonPosition, "Probeer"))
+        {
+            a_controller.AccessToController.CheckForLives();
+            //Enkel bij invoer van 1 karakter toelating geven om te spelen
+            if (a.UserInput.Length == 0 || a.UserInput.Length > 1 )
+            {
+                //Spawnt een foto die hangende man laat zien
+                a_controller.AccessToController.SpawnFoto();
+                a_controller.AccessToModel.Lives--;
+            }
+            else
+            {
+                //de checkCharacter gaan aanspreken om elke karakter in het woord te checken op de invoer, bij dezelfde karakter deze op die plaats zetten
+                a_controller.AccessToController.checkCharacter(a.UserInput[0]);
+                //Leegmaken van de user input na elke invoer van een letter
+                a.UserInput = GUI.TextField(textFieldPosition,"");
+            }
+        }
+
+        if (a_controller.AccessToModel.Still_alive == false)
+        {
+            //instantieer een nieuwe button (enkel hier toepasbaar door GUI laag enkel 1 maal bereikbaar
+            if (GUI.Button(new Rect(300, 200, 200, 100), "Play Again"))
+            {
+                a_controller.AccessToController.GenerateRandomWord();
+                a_controller.AccessToController.CounterZero();
+                a_controller.AccessToModel.Time = 61;
+                GameObject.FindWithTag("fotos").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("");
+            }
+        }
+    }
+}
